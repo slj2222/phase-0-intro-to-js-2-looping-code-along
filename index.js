373a6951f51d2f12cedf453b2dde6c4a5fd8e0e1
Spencer Johnson <sljohnson2222@gmail.com>
2022-01-20 10:28:10 -0600

    Completed assignemt

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    let messages = [];
+    for(let i = 0; i < names.length; i ++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+
+function countDown (positiveNumber) {
+    while(positiveNumber >= 0); {
+    console.log(positiveNumber);
+    positiveNumber--;
+    }
+}
